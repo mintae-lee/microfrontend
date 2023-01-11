9bea622291db78d9cf5388d3b473c05d2976d8fb
Mintae Lee <k-konno@sensyn-robotics.com>
2023-01-11 14:28:07 +0900

    add react host

---
+++ react-host/src/App.jsx
@@ -0,0 +1,20 @@
+import React, { useRef, useEffect } from "react";
+import ReactDOM from "react-dom";
+import CounterWrapper from "remote/CounterWrapper"
+import "./index.scss";
+
+const App = () => {
+  const divRef = useRef(null);
+  
+  useEffect(() => {
+    CounterWrapper(divRef.current);
+  }, []);
+
+  return (
+    <div className="mt-10 text-3xl mx-auto max-w-6xl">
+      <div>Name: react-host</div>
+      <div ref={ divRef }></div>
+    </div>
+  );
+}
+ReactDOM.render(<App />, document.getElementById("app"));
