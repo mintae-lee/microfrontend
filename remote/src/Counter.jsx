41522a7bc6738ea974039db5b7cfd53aa96b11a1
Mintae Lee <k-konno@sensyn-robotics.com>
2023-01-11 14:29:53 +0900

    Counter css style

--- remote/src/Counter.jsx
+++ remote/src/Counter.jsx
@@ -4,7 +4,7 @@ export default () => {
   const [count, setCount] = createSignal(0);
   
   return (
-    <div>
+    <div class="bg-blue-900 text-white p-5">
       <div>Count = { count() }</div>
       <button onClick={ () => setCount(count() + 1) }>Add</button>
     </div>
